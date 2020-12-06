364a98cfe0557022dbf7da09303796667aa8ef29
Mina Mina <minakhamesi@gmail.com>
2020-12-06 07:19:34 -0500

    Added hard folder with four number sum question

---
+++ Hard/FourNumberSum.py
@@ -0,0 +1,26 @@
+class SuffixTrie:
+    def __init__(self, string):
+        self.root = {}
+        self.endSymbol = "*"
+        self.populateSuffixTrieFrom(string)
+
+    def populateSuffixTrieFrom(self, string):
+		
+        for i in reversed(range(len(string))):
+			current = self.root
+			for j in range(i,len(string)):
+				char = string[j]
+				if char not in current:
+					current[char] = {}
+				current = current[char]
+			current[self.endSymbol] = True
+					
+
+    def contains(self, string):
+		current = self.root
+        for char in string:
+			if char not in current:
+				return False
+			current = current[char]
+		return self.endSymbol in current
+			
