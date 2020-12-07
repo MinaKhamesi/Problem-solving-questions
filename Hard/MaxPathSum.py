11a1df241f2d61255eb079b0e3d50d62ad552f1e
Mina Mina <minakhamesi@gmail.com>
2020-12-07 10:29:10 -0500

    solution to MaxPathSum had a bug, it didnt consider the max being the node by itself, fixed it

--- Hard/MaxPathSum.py
+++ Hard/MaxPathSum.py
@@ -10,10 +10,10 @@ def calculateSums(node, info):
 	leftSum = calculateSums(node.left, info)
 	rightSum = calculateSums(node.right, info)
 	
-	currentPathMaxSum = max((leftSum + rightSum + node.value), (leftSum + node.value), (rightSum + node.value))
+	currentPathMaxSum = max((leftSum + rightSum + node.value), (leftSum + node.value), (rightSum + node.value), node.value)
 	
 	info['maxSum'] = max(info['maxSum'], currentPathMaxSum)
 	
-	maxBranchSum = max(leftSum, rightSum) + node.value
+	maxBranchSum = max(max(leftSum, rightSum) + node.value , node.value)
 	
 	return maxBranchSum
