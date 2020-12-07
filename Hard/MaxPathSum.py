a8032a25dca97d37a71bf628ba8fb48cac0b1ce5
Mina Mina <minakhamesi@gmail.com>
2020-12-07 10:00:48 -0500

    max path sum question added

---
+++ Hard/MaxPathSum.py
@@ -0,0 +1,19 @@
+def maxPathSum(tree):
+	if tree is None: return 0
+    info = {'maxSum':float('-inf')}
+	calculateSums(tree, info)
+	return info['maxSum']
+
+def calculateSums(node, info):
+	if node is None: return 0
+	
+	leftSum = calculateSums(node.left, info)
+	rightSum = calculateSums(node.right, info)
+	
+	currentPathMaxSum = max((leftSum + rightSum + node.value), (leftSum + node.value), (rightSum + node.value))
+	
+	info['maxSum'] = max(info['maxSum'], currentPathMaxSum)
+	
+	maxBranchSum = max(leftSum, rightSum) + node.value
+	
+	return maxBranchSum
