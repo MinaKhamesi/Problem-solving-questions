1edbd6019d19381162564b1117d9f1fa49810e9e
Mina Mina <minakhamesi@gmail.com>
2020-12-26 11:07:45 -0500

    two other solution for multi string problem

--- Hard/MultiStringSearch.py
+++ Hard/MultiStringSearch.py
@@ -21,3 +21,62 @@ def isInBigHelper(big,small,start):
         rightBig -= 1
     return True
 
+##SOLUTION2
+## O(b^2 + ns) time | O(b^2 + n) space
+def multiStringSearch2(bigString, smallStrings):
+    trie = SuffixTrie(bigString)
+    return [trie.contains(string) for string in smallStrings]
+
+class SuffixTrie:
+    def __init__(self, string):
+        self.root = {}
+        self.buildSuffixTrie(string)
+    def buildSuffixTrie(self, string):
+        for i in range(len(string)):
+            current = self.root
+            for j in range(i , len(string)):
+                if string[j] not in current:
+                    current[string[j]] = {}
+                current = current[string[j]]
+            
+    def contains(self,string):
+        current = self.root
+        for char in string:
+            if char not in current: return False
+            current = current[char]
+        return True
+
+##SOLUTION3
+#O( ns + bs ) time | O(ns) space
+def multiStringSearch3(bigString, smallStrings):
+    trie = Trie(smallStrings)
+    stringsFound = {}
+    for i in range(len(bigString)):
+        searchThrough(bigString, i , stringsFound, trie)
+    return [string in stringsFound for string in smallStrings]
+
+class Trie:
+    def __init__(self, strings):
+        self.root = {}
+        self.buildTrie(strings)
+
+    def buildTrie(self, strings):
+        for string in strings:
+            current = self.root
+            for char in string:
+                if char not in current:
+                    current[char] = {}
+                current = current[char]
+            current['*'] = string
+
+def searchThrough(bigString, startIdx, stringsFound, trie):
+    current = trie.root
+    for i in range(startIdx , len(bigString)):
+        if bigString[i] not in current:
+            break
+        else:
+            current = current[bigString[i]]
+            if '*' in current:
+                stringsFound[current['*']] = True
+
+
