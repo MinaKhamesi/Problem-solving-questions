5c1ec17a3917c72b306c978f9e0137cf446683e1
Mina Mina <minakhamesi@gmail.com>
2021-01-03 14:55:41 -0500

    max profit question with a more optimal solution

--- Very Hard/MaxProfitWithKTransaction.py
+++ Very Hard/MaxProfitWithKTransaction.py
@@ -11,3 +11,20 @@ def maxProfitWithKTransaction( prices , k):
 
     return maxProfits[k][-1]
 
+def maxProfitWithKTransaction2( prices , k ):
+    if not len(prices): return 0
+    odd = [ 0 for day in prices]
+    even = [0 for day in prices]
+    for transaction in range(1 , k + 1):
+        if transaction % 2 == 1:
+            current = even
+            prev = odd
+        else:
+            current = odd
+            prev = even
+        maxThusFar = float('-inf')
+        for day in range(1, len(prices) ):
+            maxThusFar = max(maxThusFar , prev[day - 1] - prices[day - 1])
+            current[day] = max(current[day - 1] , prices[day] + maxThusFar)
+    return even[-1] if k % 2 == 1 else odd[-1]
+
